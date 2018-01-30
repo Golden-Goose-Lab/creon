262f029e4979670067f394c2e91ba2a8e1fa239e
Parkayun <iamparkayun@gmail.com>
2018-01-30 15:12:43 +0900

    Add get_all_codes

---
+++ c.py
@@ -0,0 +1,5 @@
+from creon.core import Creon
+
+
+creon = Creon()
+print(type(creon.utils))
--- creon/core.py
+++ creon/core.py
@@ -3,7 +3,14 @@ from win32com import client
 
 class Creon:
 
+    __codes__ = None
     __utils__ = None
+
+    @property
+    def codes(self) -> client.CDispatch:
+        if self.__codes__ is None:
+            self.__codes__ = client.Dispatch("CpUtil.CpCodeMgr")
+        return self.__codes__
 
     @property
     def utils(self) -> client.CDispatch:
@@ -15,3 +22,21 @@ class Creon:
     @property
     def account_numbers(self) -> tuple:
         return self.utils.AccountNumber
+
+    def get_all_codes(self, category: str, with_name: bool = False) -> tuple:
+        category = category.lower()
+        if category == 'kospi':
+            section = 1
+        elif category == 'kosdaq':
+            section = 2
+        else:
+            raise ValueError
+
+        codes = self.codes.GetStockListByMarket(section)
+
+        if with_name:
+            results = []
+            for code in codes:
+                results.append((code, self.codes.CodeToName(code)))
+            return tuple(results)
+        return codes
